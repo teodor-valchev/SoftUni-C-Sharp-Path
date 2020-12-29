1548c1664273f20afbba72ea16122f379b08c2c2
Teo <teo.thenx@gmail.com>
2020-12-29 18:41:16 +0200

    Adedd Excercise-ceic02.Conditional Statements - Exercise = 4;

---
+++ Programing-Basics/Excercise/02.Conditional Statements - Exercise/04. Metric Converter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04._Metric_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+            string result = Console.ReadLine();
+
+            if (result=="m")
+            {
+                double result3 = a /1000;
+               
+                Console.WriteLine($"{ result3:f3}");
+                
+            }
+            else if (result=="cm")
+            {
+                double result2 = a * 100;
+                Console.WriteLine($"{ result2:f3}");
+            }
+            else if (result == "mm")
+            {
+                double result4 = a * 10;
+                Console.WriteLine($"{ result4:f3}");
+            }
+
+        }
+    }
+}
