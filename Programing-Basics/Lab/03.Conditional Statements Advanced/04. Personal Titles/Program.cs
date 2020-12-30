3a6c2daeb78eecf1a4d68e8e0e446ab6e25ef65a
Teo <teo.thenx@gmail.com>
2020-12-30 20:14:57 +0200

    Added Conditional Statements Advanced - till exc 6

---
+++ Programing-Basics/Lab/03.Conditional Statements Advanced/04. Personal Titles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04._Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = int.Parse(Console.ReadLine());
+            char type = char.Parse(Console.ReadLine());
+
+            if (age>=16)
+            {
+                if (type=='m')
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else if (type=='f')
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else if (age<=16)
+            {
+                if (type=='m')
+                {
+                    Console.WriteLine("Master");
+                }
+                else if (type=='f')
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
---
+++ Programing-Basics/Lab/03.Conditional Statements Advanced/06. Number in Range/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06._Number_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (  a < 100 && a>-100)
+            {
+                if (a!=0)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+
+            }
+        }
+    }
+}
