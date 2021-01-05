71a66d715188dcbd920fd84c1ba9723b5208dfaa
Teo <teo.thenx@gmail.com>
2021-01-05 15:16:21 +0200

    Added forLoop Lab

---
+++ Programing-Basics/Lab/04.For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+                else if (number<minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
