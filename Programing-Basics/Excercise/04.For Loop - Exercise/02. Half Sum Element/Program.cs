ad6c51738f537c3d564b8ea646ffa3ad6c63686d
Teo <teo.thenx@gmail.com>
2021-01-06 18:14:08 +0200

    Added Excercise/04.For Loop - Exercise/.vs/04.For Loop - Exercise/DesignTimeBuild/.dtbcache.v2

---
+++ Programing-Basics/Excercise/04.For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                int numbersum = number + number;
+
+                sum += number;
+
+                if (number>max)
+                {
+                    max = number;
+                }
+               
+            }
+
+            if (sum-max==max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {sum-max}");
+            }
+            
+
+        }
+    }
+}
---
+++ Programing-Basics/Excercise/04.For Loop - Exercise/03. Histogram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _03._Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                if (numbers < 200)
+                {
+                    p1++;
+                }
+                else if (numbers >= 200 && numbers <= 399)
+                {
+                    p2++;
+                }
+                else if (numbers >= 400 && numbers <= 599)
+                {
+                    p3++;
+                }
+                else if (numbers >= 600 && numbers <= 799)
+                {
+                    p4++;
+                }
+                else if (numbers >= 800)
+                {
+                    p5++;
+                }
+
+            }
+            Console.WriteLine($"p1 = {1.0 * p1 / n * 100:f2}%");
+            Console.WriteLine($"p2 = {1.0 * p2 / n * 100:f2}%");
+            Console.WriteLine($"p3 = {1.0 * p3 / n * 100:f2}%");
+            Console.WriteLine($"p4 = {1.0 * p4 / n * 100:f2}%");
+            Console.WriteLine($"p5 = {1.0 * p5 / n * 100:f2}%");
+        }
+    }
+}
