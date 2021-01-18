8de1cac6ae8d914d7e8dc0687c947217bea7f5f1
Teo <teo.thenx@gmail.com>
2021-01-18 19:46:34 +0200

    add

---
+++ Programing-Basics/Lab/06.Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int validCombination = 0;
+            for (int i = 0; i <= num; i++)
+            {
+                for (int j = 0; i <=num ; j++)
+                {
+                    for (int k = 0; i <= num; k++)
+                    {
+                        
+                        if (i+j+k==num)
+                        {
+                            validCombination++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(validCombination);
+        }
+    }
+}
