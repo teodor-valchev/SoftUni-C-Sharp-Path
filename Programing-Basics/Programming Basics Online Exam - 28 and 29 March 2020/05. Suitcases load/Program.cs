9970bc450d155d392b1849bfdfc56ee6bf921782
Teo <teo.thenx@gmail.com>
2021-01-26 21:20:41 +0200

    exams

--- Programing-Basics/Programming Basics Online Exam - 28 and 29 March 2020/05. Suitcases load/Program.cs
+++ Programing-Basics/Programming Basics Online Exam - 28 and 29 March 2020/05. Suitcases load/Program.cs
@@ -6,7 +6,47 @@ namespace _05._Suitcases_load
     {
         static void Main(string[] args)
         {
-         
+            double load = double.Parse(Console.ReadLine());
+            int suitcase = 0;
+            bool goalReached = false;
+            while (goalReached==false)
+            {
+                string input = Console.ReadLine();
+                
+                if (input=="End")
+                {
+                    goalReached = true;
+                    break;
+
+                }
+                double suitcaseVolume = double.Parse(input);
+                suitcase++;
+                if (suitcase%3==0)
+                {
+                    suitcaseVolume += 7.2;
+                }
+                
+                double totalBagage = load - suitcaseVolume;
+
+                if (load<totalBagage)
+                {
+                    break;
+                }
+                
+
+
+            }
+            if (goalReached)
+            {
+                Console.WriteLine($"Congratulations! All suitcases are loaded!");
+                Console.WriteLine($"Statistic: {suitcase} suitcases loaded.");
+            }
+            else
+            {
+                Console.WriteLine("No more space!");
+                Console.WriteLine($"Statistic: {suitcase+1} suitcases loaded.");
+            }
+
         }
     }
 }
---
+++ Programing-Basics/Programming Basics Online Exam - 28 and 29 March 2020/06. Tournament of Christmas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Tournament_of_Christmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
