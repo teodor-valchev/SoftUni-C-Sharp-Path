4b2ded3c92689298d66539c82a6a27824703b49c
Teo <teo.thenx@gmail.com>
2021-01-27 15:48:08 +0200

    add

--- Programing-Basics/Programming Basics Online Exam - 28 and 29 March 2020/06. Tournament of Christmas/Program.cs
+++ Programing-Basics/Programming Basics Online Exam - 28 and 29 March 2020/06. Tournament of Christmas/Program.cs
@@ -6,7 +6,57 @@ namespace _06._Tournament_of_Christmas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dayCount = int.Parse(Console.ReadLine());
+
+            int dayWin = 0;
+            int dayLose = 0;
+            double allProfit = 0;
+
+            for (int i = 1; i <= dayCount; i++)
+            {
+                int countWinGames = 0;
+                int countLoseGames = 0;
+                double dayProfit = 0;
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "Finish")
+                    {
+                        break;
+                    }
+                    string result = Console.ReadLine();
+                    if (result == "win")
+                    {
+                        countWinGames++;
+                        dayProfit += 20;
+                    }
+                    else if (result == "lose")
+                    {
+                        countLoseGames++;
+                    }
+                }
+                if (countWinGames > countLoseGames)
+                {
+                    dayProfit *= 1.1;
+                    dayWin++;
+                }
+                else
+                {
+                    dayLose++;
+                }
+                allProfit += dayProfit;
+            }
+            if (dayWin > dayLose)
+            {
+                allProfit *= 1.2;
+                Console.WriteLine($"You won the tournament! Total raised money: {allProfit:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {allProfit:F2}");
+            }
+
+
         }
     }
 }
