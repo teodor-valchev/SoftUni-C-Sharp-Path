23fa1d012d34d1b6ed96928db230e905f585367d
Teo <teo.thenx@gmail.com>
2021-01-28 17:04:14 +0200

    Added basic syntax lab

---
+++ C#-Fundamentals/Lab/0.1 Basic Syntax, Conditional Statements and Loops/13. Holidays Between Two Dates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13._Holidays_Between_Two_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
