3df173853e7c2e0b18e5c9798ecd9dc533abd0da
Teo <teo.thenx@gmail.com>
2021-02-07 13:38:52 +0200

    added lab data

---
+++ C#-Fundamentals/Lab/02. Data Types and Variables/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kilometers = meters / 1000D;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
---
+++ C#-Fundamentals/Lab/02. Data Types and Variables/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pounds = int.Parse(Console.ReadLine());
+            double dollars = pounds *1.31;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
