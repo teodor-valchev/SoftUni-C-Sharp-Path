87beded329089091a6c738441aee5650a3193269
Teo <teo.thenx@gmail.com>
2021-03-04 22:03:17 +0200

    added lab 3

---
+++ C#-Fundamentals/Lab/05.List/03. Merging Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().
+                 Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+            List<int> secondList = Console.ReadLine().
+               Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            List<double> result = new List<double>();
+
+            for (int i = 0; i < Math.Min(firstList.Count,secondList.Count); i++)
+            {
+                result.Add(firstList[i]);
+                result.Add(secondList[i]);
+            }
+
+
+            for (int i = Math.Min(firstList.Count, secondList.Count); i < Math.Max(firstList.Count, secondList.Count); i++)
+            {
+                if (i >= firstList.Count)
+                {
+                    result.Add(secondList[i]);
+                }
+
+                else
+                {
+                    result.Add(firstList[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",result));
+
+        }
+    }
+}
