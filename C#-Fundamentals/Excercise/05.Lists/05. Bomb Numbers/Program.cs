6d15f8e2102a09ea8a8d476d3e162eb68ce20cde
Teo <teo.thenx@gmail.com>
2021-03-15 11:11:03 +0200

    add

---
+++ C#-Fundamentals/Excercise/05.Lists/05. Bomb Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int[] bombProp = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int bomb = bombProp[0];
+            int power = bombProp[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber == bomb)
+                {
+                    int startIndex = i - power;
+                    int endIndex = i + power;
+
+                    if (startIndex<0)
+                    {
+                        startIndex = 0;
+                    }
+
+                    if (endIndex>numbers.Count - 1)
+                    {
+                        endIndex = numbers.Count - 1; // nqma da izleem ot granicite 
+                    }
+
+                    int endIndexToRemove = endIndex - startIndex + 1;
+                    numbers.RemoveRange(startIndex, endIndexToRemove);
+
+                    i = startIndex - 1; // няма да изкара индекс out of range
+                }
+            }
+
+            Console.WriteLine(numbers.Sum());
+
+        }
+    }
+}
---
+++ C#-Fundamentals/Excercise/05.Lists/10. SoftUni Course Planning/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._SoftUni_Course_Planning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lessons = Console.ReadLine()
+                 .Split(", ")
+                 .ToList();
+            string command = Console.ReadLine();
+
+            while (command != "course start")
+            {
+                string[] cmdArg = command.Split(":").ToArray();
+                string firstCommand = cmdArg[0];
+                string lessonTitle = cmdArg[1];
+
+                if (firstCommand == "Add")
+                {
+                    if (!lessons.Contains(lessonTitle))
+                    {
+                        lessons.Add(lessonTitle);
+                    }
+                }
+                else if (firstCommand == "Insert")
+                {
+                    int index = int.Parse(cmdArg[2]);
+
+                    if (!lessons.Contains(lessonTitle))
+                    {
+                        lessons.Insert(index, lessonTitle);
+                    }
+                }
+                else if (firstCommand == "Remove")
+                {
+                    lessons.Remove(lessonTitle);
+                }
+                else if (firstCommand == "Swap")
+                {
+                    string secondLessonTitle = cmdArg[2];
+
+                    int indexOfFirstLesson = lessons.IndexOf(lessonTitle);
+                    int indexOfSecondLesson = lessons.IndexOf(secondLessonTitle);
+
+                    if (indexOfFirstLesson != -1 && indexOfSecondLesson != -1)
+                    {
+                        lessons[indexOfFirstLesson] = secondLessonTitle;
+                        lessons[indexOfSecondLesson] = lessonTitle;
+
+                        string firstLessonExcercise = $"{lessonTitle}-Exercise";
+                        int indexOfFirstExcercise = indexOfFirstLesson + 1;
+
+                        if (indexOfFirstLesson < lessons.Count && lessons[indexOfFirstExcercise] == firstLessonExcercise)
+                        {
+                            lessons.RemoveAt(indexOfFirstExcercise);
+                            indexOfFirstExcercise = lessons.IndexOf(lessonTitle);
+                            lessons.Insert(indexOfFirstExcercise, firstLessonExcercise);
+                        }
+
+                        string secondLessonEsxxercise = $"{secondLessonTitle}-Exercise";
+                        int indexOfSecondExcercise = indexOfSecondLesson + 1 ;//oo
+
+                        if (indexOfSecondExcercise < lessons.Count && lessons[indexOfSecondExcercise] == secondLessonEsxxercise)
+
+                        {
+                            lessons.RemoveAt(indexOfSecondLesson + 1);
+                            indexOfSecondLesson = lessons.IndexOf(secondLessonTitle);
+                            lessons.Insert(indexOfSecondLesson + 1, secondLessonEsxxercise);
+                        }
+                    }
+                }
+                else if (firstCommand == "Exercise")
+                {
+                    int index = lessons.IndexOf(lessonTitle);
+                    string excerciese = $"{lessonTitle} - Exercise";
+
+                    bool isThereAreLesson = lessons.Contains(lessonTitle);
+                    bool isThereExercise = lessons.Contains(excerciese);
+
+                    if (isThereAreLesson && !isThereExercise)
+                    {
+                        lessons.Insert(index + 1, excerciese);
+
+                    }
+                    else if (!isThereAreLesson)
+                    {
+                        lessons.Add(lessonTitle);
+                        lessons.Add(excerciese);
+                    }
+
+                }
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < lessons.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{lessons[i]}"); // i +1 за да не принти от 0
+            }
+        }
+    }
+}
