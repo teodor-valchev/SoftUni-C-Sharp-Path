3d977b41e63336a3c9328ab62d2583b2cdfb2d0b
Teo <teo.thenx@gmail.com>
2021-03-22 15:46:08 +0200

    sa

---
+++ C#-Fundamentals/Excercise/06.Objects and Classes/01. Advertisement Message/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+
+            string[] city = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                string phrase = phrases[rnd.Next(0, phrases.Length)];
+                string place = events[rnd.Next(0, events.Length)];
+                string author = authors[rnd.Next(0, authors.Length)];
+                string hometown = city[rnd.Next(0, city.Length)];
+
+                Console.WriteLine($"{phrase} {place} {author} – {hometown}.");
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/Excercise/06.Objects and Classes/03. Articles 2.0/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Articles_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(", ");
+           
+                string title = tokens[0];
+                string content = tokens[1];
+                string author = tokens[2];
+
+                Article article = new Article(title, content, author);
+                articles.Add(article);
+            }
+
+            string criteria = Console.ReadLine();
+
+            if (criteria == "title")
+            {
+              articles =   articles.OrderBy(x => x.Title)
+                    .ToList();// и двете със сорт вършат едно и също
+            }
+            else if (criteria == "content")
+            {
+               articles.Sort((c1,c2)=> c1.Content.CompareTo(c2.Content));
+            }
+            else if (criteria == "author")
+            {
+               articles =  articles.OrderBy(x => x.Author)
+                    .ToList();
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine,articles));
+        }
+    }
+}
