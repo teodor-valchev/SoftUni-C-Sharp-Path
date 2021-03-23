6f4102e6e2f32e6bf4eaa3cb44b9e31c6bcdcd8d
Teo <teo.thenx@gmail.com>
2021-03-23 16:06:26 +0200

    add

---
+++ C#-Fundamentals/Excercise/06.Objects and Classes/04. Students/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split();
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                double grade = double.Parse(tokens[2]);
+                Student student = new Student(firstName, lastName, grade);
+
+                students.Add(student);
+            }
+
+           students= students.OrderByDescending(x => x.Grade).ToList();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student (string firstName,string lastNime,double grade)
+        {
+            FirstName = firstName;
+            LastName = lastNime;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade}";
+        }
+    }
+}
