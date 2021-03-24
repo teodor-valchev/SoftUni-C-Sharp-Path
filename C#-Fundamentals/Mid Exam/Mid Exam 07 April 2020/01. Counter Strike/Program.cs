88e1d6774293113d9b036c5f628e38122b4e067d
Teo <teo.thenx@gmail.com>
2021-03-24 13:02:47 +0200

    added

---
+++ C#-Fundamentals/Mid Exam/Mid Exam 07 April 2020/01. Counter Strike/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            int wins = 0;
+            int battle = 0;
+            bool notEnoughEnergy = false;
+
+            while (command != "End of battle")
+            {
+                battle++;
+                int distance = int.Parse(command);
+                energy -= distance;
+                wins++;
+
+                if (battle % 3 == 0)
+                {
+                    energy += wins;
+                }
+
+                if (energy < 0)
+                {
+                    energy = 0;
+                    notEnoughEnergy = true;
+                    break;
+                }
+
+
+
+                command = Console.ReadLine();
+            }
+
+            if (notEnoughEnergy)
+            {
+                Console.WriteLine($"Not enough energy! Game ends with { wins - 1 } won battles and { energy} energy");
+            }
+            else
+            {
+                Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals/Mid Exam/Mid Exam 07 April 2020/02. Shoot for the Win/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Shoot_for_the_Win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int shotTarggets = 0;
+
+            string comand = Console.ReadLine();
+
+            while (comand != "End")
+            {
+                int index = int.Parse(comand);
+
+                if (index < 0 || index >= targets.Length || targets[index] == -1)
+                {
+                    comand = Console.ReadLine();
+                    continue;
+                }
+
+                int shotTarget = targets[index];
+                targets[index] = -1;
+                shotTarggets++;
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    if (targets[i] == -1)
+                    {
+                        continue;
+                    }
+                    if (targets[i] > shotTarget)
+                    {
+                        targets[i] -= shotTarget;
+                    }
+                    else
+                    {
+                        targets[i] += shotTarget;
+                    }
+                }
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    int currentTarget = targets[i];
+
+
+                }
+
+
+                comand = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Shot targets: {shotTarggets} -> {string.Join(" ", targets)}");
+        }
+    }
+}
