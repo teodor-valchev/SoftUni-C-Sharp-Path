67f778e741c4bc659e1b7864f12acb073c42ab9f
Teo <teo.thenx@gmail.com>
2021-03-24 14:59:05 +0200

    exam 2

---
+++ C#-Fundamentals/Mid Exam/Mid Exam Alex/02. Shopping List/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+List<string> groceres = Console.ReadLine()
+                 .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command!= "Go Shopping!")
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string input = tokens[0];
+                string item = tokens[1];
+              
+
+                switch (input)
+                {
+                    case "Urgent":
+                        if (!groceres.Contains(item))
+                        {
+                            groceres.Add(item);
+                        }
+                        break;
+                    case "Unnecessary":
+                        if (groceres.Contains(item))
+                        {
+                            groceres.Remove(item);
+                        }
+                        else
+                        {
+                            command = Console.ReadLine();
+                            continue;
+                        }
+
+                        break;
+                    case "Correct":
+                        string newItem = tokens[2];
+                        if (groceres.Contains(item))
+                        {
+
+                            int index = groceres.FindIndex(x => x == item);
+                            groceres.RemoveAt(index);
+                            groceres.Insert(index, newItem);
+                            
+                        }
+                        else
+                        {
+                            command = Console.ReadLine();
+                            continue;
+                        }
+                        break;
+                    case "Rearrange ":
+                        if (groceres.Contains(item))
+                        {
+                            groceres.Remove(item);
+                            groceres.Add(item);
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(",",groceres));
+        }
+    }
+}
---
+++ C#-Fundamentals/Mid Exam/Mid Exam Alex/03. Heart Delivery/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Heart_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numberOfHearts = Console.ReadLine().Split("@", StringSplitOptions.RemoveEmptyEntries);
+
+            string command = Console.ReadLine();
+
+            while (command!="Love!")
+            {
+              
+
+
+                command = Console.ReadLine();
+            }
+                
+        }
+    }
+}
