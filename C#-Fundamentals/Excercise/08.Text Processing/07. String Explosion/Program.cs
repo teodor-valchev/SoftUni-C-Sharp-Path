0a8ab23269c567ba1bc4079abd05b6b9370c8348
Teo <teo.thenx@gmail.com>
2021-04-05 13:12:57 +0300

    all Excercise/08

---
+++ C#-Fundamentals/Excercise/08.Text Processing/07. String Explosion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var field = Console.ReadLine();
+            int bomb = 0;
+            for (int i = 0; i < field.Length; i++)
+            {
+                var currentChar = field[i];
+
+                if (currentChar == '>')
+                {
+                    bomb += int.Parse(field[i + 1].ToString());
+                    continue;
+                }
+
+                if (bomb > 0)
+                {
+                    field = field.Remove(i, 1);
+                    i--;// za da ne se burkat index
+                    bomb--;
+                }
+            }
+
+            Console.WriteLine(field);
+        }
+    }
+}
---
+++ C#-Fundamentals/Excercise/08.Text Processing/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _08._Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double result = 0;
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string current = input[i];//tekushtiq simvol
+                char firsLetter = current[0];//vzimame purviq simvol
+                char lastLetter = current[current.Length - 1];//vzimame posledniq simvol
+
+                double number = double.Parse(current.Substring(1, current.Length - 2));
+
+                int firstElementIndex = alphabet.IndexOf(char.ToUpper(firsLetter));
+                int secondElementIndex = alphabet.IndexOf(char.ToUpper(lastLetter));
+
+                if ((int)firsLetter >= 65 && (int)firsLetter <= 90)
+                {
+                    number = number / (firstElementIndex + 1);//za da ne delim na 0
+
+                }
+                else
+                {
+                    number = number * (firstElementIndex + 1);
+                }
+
+                if ((int)lastLetter >= 65 && (int)lastLetter <= 90)
+                {
+                    number = number - (secondElementIndex + 1);//za da ne delim na 0
+
+                }
+                else
+                {
+                    number = number + (secondElementIndex + 1);
+                }
+
+                result += number;
+
+            }
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
+
