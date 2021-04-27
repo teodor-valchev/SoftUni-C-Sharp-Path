cb12c76eefdd898ebfbca5d0357c7970af8ed07e
Reazer987 <teo.thenx@gmail.com>
2021-04-27 16:59:01 +0300

    reg ex lab

---
+++ C#-Fundamentals/Lab/09.Reg Ex/02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\+359([ -])2(\2)(\d{3})(\2)(\d{4}))\b";
+            Regex regex = new Regex(pattern);
+
+            var input = (Console.ReadLine());
+
+            var matches = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ",matches));
+        }
+    }
+}
---
+++ C#-Fundamentals/Lab/09.Reg Ex/03. Match Dates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>[0-2][1-9])([-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b";
+
+
+            var input = Console.ReadLine();
+            var dates = Regex.Matches(input, pattern);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+            Console.WriteLine(string.Join(" ",dates));
+        }
+    }
+}
