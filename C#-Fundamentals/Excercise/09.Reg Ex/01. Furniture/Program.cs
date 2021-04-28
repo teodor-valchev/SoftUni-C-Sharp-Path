9d64ee6769036c2dac17c37e80541a931d4c8e58
Reazer987 <teo.thenx@gmail.com>
2021-04-28 18:49:13 +0300

    адд

---
+++ C#-Fundamentals/Excercise/09.Reg Ex/01. Furniture/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>([A-Za-z]+)<<(\d+.?\d*)!(\d+)";
+
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+
+
+
+            Console.WriteLine("Bought furniture:");
+            double sum = 0;
+
+            while (input != "Purchase")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string furniture = match.Groups[1].Value;
+                    double price = double.Parse(match.Groups[2].Value);
+                    int quantity = int.Parse(match.Groups[3].Value);
+
+                    Console.WriteLine(furniture);
+                    sum += price * quantity;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
