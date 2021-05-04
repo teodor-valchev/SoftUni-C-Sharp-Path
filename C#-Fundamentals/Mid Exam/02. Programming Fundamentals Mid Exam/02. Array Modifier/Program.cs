b50a40aecd07a1ec17d7da1ca248f76a70415a42
Reazer987 <teo.thenx@gmail.com>
2021-05-04 20:55:34 +0300

    d

---
+++ C#-Fundamentals/Mid Exam/02. Programming Fundamentals Mid Exam/02. Array Modifier/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Array_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] cmdArg = command.Split();
+                string name = cmdArg[0];
+               
+
+                if (name =="swap")
+                {
+                    int firstIndex = int.Parse(cmdArg[1]);
+                    int secondIndex = int.Parse(cmdArg[2]);
+
+                    int temp = numbers[firstIndex];
+
+                    numbers[firstIndex] = numbers[secondIndex];
+                    numbers[secondIndex] = temp;
+                    temp = secondIndex;
+                   
+                }
+                else if (name == "multiply")
+                {
+                    int firstIndex = int.Parse(cmdArg[1]);
+                    int secondIndex = int.Parse(cmdArg[2]);
+
+                    int saveIndex = numbers[1];
+                  int result =  numbers[firstIndex] *= numbers[secondIndex];
+                    saveIndex=result;
+
+
+                }
+                else if (name =="decrease")
+                {
+                    for (int i = 0; i <= numbers.Length-1; i++)
+                    {
+                        numbers[i] -= 1;
+                        
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", " , numbers));
+        }
+    }
+}
---
+++ C#-Fundamentals/Mid Exam/02. Programming Fundamentals Mid Exam/03. Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+           double avarage =  numbers.Average();
+
+         int [] nums   =   numbers.Where(x=>x > avarage).ToArray();
+
+            int count = 0;
+            foreach (var number in nums.OrderByDescending(x=>x))
+            {
+                Console.WriteLine(number);
+                count++;
+                if (count == 5)
+                {
+                    break;
+                }
+            }
+            if (avarage>=nums.Length)
+            {
+                Console.WriteLine("No");
+            }
+           
+        }
+    }
+}
