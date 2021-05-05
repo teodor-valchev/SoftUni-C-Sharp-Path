f7e445c6b1b7bb1f80d706ecf39fbec04f868907
Reazer987 <teo.thenx@gmail.com>
2021-05-05 18:44:08 +0300

    added till 2

---
+++ C#-Fundamentals/Mid Exam/01. Programming Fundamentals Mid Exam Retake/01. Computer Store/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _01._Computer_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string command = Console.ReadLine();
+            double totalPricewithoutTaxes = 0;
+
+            while (command != "special" && command != "regular")
+            {
+                double price = double.Parse(command);
+
+
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    command = Console.ReadLine();
+                    continue;
+                }
+                totalPricewithoutTaxes += price;
+
+                command = Console.ReadLine();
+            }
+
+
+            bool succes = false;
+
+            double discount = totalPricewithoutTaxes * 0.2;
+            double totalPrice = discount + totalPricewithoutTaxes;
+            double final = 0;
+
+            if (command == "special")
+            {
+                double extraDiscount = totalPrice * 0.1;
+                totalPrice -= extraDiscount;
+                final += totalPrice;
+            }
+          
+           
+            if (final == 0)
+            {
+                final += totalPrice;
+                
+            }
+
+            if (final>0)
+            {
+                succes = true;
+            }
+
+            if (succes == false)
+            {
+                Console.WriteLine("Invalid order!");
+
+
+            }
+
+            else if (succes == true)
+            {
+                Console.WriteLine($"Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {totalPricewithoutTaxes:f2}$");
+                Console.WriteLine($"Taxes: {discount:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {final:f2}$");
+            }
+          
+
+
+
+
+        }
+    }
+}
---
+++ C#-Fundamentals/Mid Exam/01. Programming Fundamentals Mid Exam Retake/02. The Lift/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._The_Lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+
+            int[] wagons = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+
+            int peopleLeft = 0;
+            bool HasEnoughSpace = true;
+
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                if (wagons[i] > 0)
+                {
+                    peopleLeft += wagons[i];
+                }
+
+                if (people < 4)
+                {
+                    wagons[i] += people;
+                    peopleLeft += people;
+                    people -= people;
+
+                    if (people == 0)
+                    {
+                        HasEnoughSpace = true; 
+                        break;
+                    }
+
+                }
+
+                wagons[i] += 4;
+
+                if (wagons[i] >= 4)
+                {
+                    wagons[i] = 4;
+                }
+                people -= wagons[i];
+
+                if (people > 0)
+                {
+                    HasEnoughSpace = false;
+                }
+                else if (people == 0)
+                {
+                    HasEnoughSpace = true;
+                }
+
+
+            }
+
+            if (HasEnoughSpace)
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", wagons));
+            }
+            else
+            {
+                Console.WriteLine($"There isn't enough space! {people + peopleLeft} people in a queue!");
+                Console.WriteLine(string.Join(" ", wagons));
+            }
+
+
+        }
+    }
+}
