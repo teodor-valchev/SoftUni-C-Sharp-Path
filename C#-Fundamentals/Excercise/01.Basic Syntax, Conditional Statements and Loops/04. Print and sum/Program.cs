24624c2e347269ef700dc6db682f0a1560ee5302
Теодор Вълчев <stu2001261001@uni-plovdiv.bg>
2021-05-18 10:09:28 +0300

    адд

--- C#-Fundamentals/Excercise/01.Basic Syntax, Conditional Statements and Loops/04. Print and sum/Program.cs
+++ C#-Fundamentals/Excercise/01.Basic Syntax, Conditional Statements and Loops/04. Print and sum/Program.cs
@@ -1,22 +1,72 @@
-﻿
-using System;
+﻿using System;
 
-namespace _04._Print_and_sum
+namespace _07._Vending_Machine
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int start = int.Parse(Console.ReadLine());
-            int end = int.Parse(Console.ReadLine());
-            int sum = 0;
-            for (int i = start; i <= end; i++)
+            string moneyRecieve = Console.ReadLine();
+            double insertedAmount = 0;
+
+            while (moneyRecieve != "Start")
             {
-                Console.Write(i + " ");
-                sum += i;
+                double currentCoin = double.Parse(moneyRecieve);
+
+                if (currentCoin == 0.1 || currentCoin == 0.2 || currentCoin == 0.5 || currentCoin == 1 || currentCoin == 2)
+                {
+                    insertedAmount += currentCoin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {currentCoin}");
+                }
+
+                moneyRecieve = Console.ReadLine();
+
             }
-            Console.WriteLine();
-            Console.WriteLine($"Sum: {sum}");
+            string product = Console.ReadLine();
+
+            while (product != "End")
+            {
+                double productPrice = 0;
+
+                switch (product)
+                {
+                    case "Nuts":
+                        productPrice = 2.0;
+                        break;
+                    case "Water":
+                        productPrice = 0.7;
+                        break;
+                    case "Crisps":
+                        productPrice = 1.5;
+                        break;
+                    case "Soda":
+                        productPrice = 0.8;
+                        break;
+                    case "Coke":
+                        productPrice = 1.0;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        product = Console.ReadLine();
+                        continue;
+                }
+                if (productPrice <= insertedAmount)
+                {
+                    insertedAmount -= productPrice;
+                    Console.WriteLine($"Purchased {product.ToLower()}");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                }
+
+                product = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Change: {insertedAmount:f2}");
         }
     }
 }
