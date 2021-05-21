3fb003647df765c093e4f1f83996486a14cf3560
Теодор Вълчев <teo.thenx@gmail.com>
2021-05-21 11:37:50 +0300

    add

---
+++ C#-Fundamentals/Mid Exam/04. Programming Fundamentals Mid Exam/01. National Court/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int person1 = int.Parse(Console.ReadLine());
+            int person2 = int.Parse(Console.ReadLine());
+            int person3 = int.Parse(Console.ReadLine());
+
+            int totalEmployees = person1 + person2 + person3;
+            int totalCountOfPeople = int.Parse(Console.ReadLine());
+
+            int hours = 0;
+            while (totalCountOfPeople > 0)
+            {
+                hours++;
+                totalCountOfPeople -= totalEmployees;
+               
+
+
+                if (hours % 4 == 0)
+                {
+                    hours++;
+                }
+
+            }
+            
+            Console.WriteLine($"Time needed: {hours}h.");
+            
+        }
+    }
+}
---
+++ C#-Fundamentals/Mid Exam/04. Programming Fundamentals Mid Exam/02. Shopping List/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceries = Console.ReadLine().Split("!", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command!= "Go Shopping!")
+            {
+                string[] tokens = command.Split();
+                string name = tokens[0];
+                string item = tokens[1];
+
+                if (name == "Urgent")
+                {
+                    if (!groceries.Contains(item))
+                    {
+                        groceries.Insert(0,item);
+                    }
+                }
+                else if (name == "Unnecessary")
+                {
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                    }
+                }
+                else if (name == "Correct")
+                {
+                    string newItem = tokens[2];
+
+                    if (groceries.Contains(item))
+                    {
+                      int index = groceries.FindIndex(x=>x ==item);
+                        groceries.RemoveAt(index);
+                        groceries.Insert(index, newItem);
+
+                    }
+                }
+                else if (name == "Rearrange")
+                {
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                        groceries.Add(item);
+
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ",groceries));
+        }
+    }
+}
