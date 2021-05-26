591a8c5453005eca3ba42f54ffbe50ac104cd3f8
Теодор Вълчев <teo.thenx@gmail.com>
2021-05-26 19:30:50 +0300

    add

---
+++ 02. C#-Fundamentals/04. Exams/02. Final Exam/02. Programming Fundamentals Final Exam/01. World Tour/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _01._World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+            while (command != "Travel")
+            {
+                string[] tokens = command.Split();
+                string name = tokens[0];
+                string[] splite = name.Split(":");
+                string name2 = splite[0];
+                
+
+                if (name == "Add")
+                {
+                    string stop = tokens[1];
+                    string[] split = stop.Split(":");
+                    int index = int.Parse(split[1]);
+                    string replace = split[2];
+
+                    if (index > 0 && index <= stops.Length - 1)
+                    {
+                        stops = stops.Insert(index, replace);
+                    }
+                    
+
+
+                }
+                else if (name == "Remove")
+                {
+                    string stop = tokens[1];
+                    string[] split = stop.Split(":");
+                    int startIndex = int.Parse(split[1]);
+                    int endIndex = int.Parse(split[2]);
+
+                    if (startIndex>=0 && endIndex<=stops.Length-1)
+                    {
+                       string first= stops.Substring(0,startIndex);
+                        string second = stops.Substring(startIndex, endIndex - startIndex);
+                        string three = stops.Substring(endIndex+1);
+
+                        stops = first + three;
+
+
+                    }
+                  
+                }
+
+                else if (name2 == "Switch")
+                {
+                    string old = splite[1];
+                    string newString = splite[2];
+
+                    if (stops.Contains(old))
+                    {
+                        stops = stops.Replace(old, newString);
+                        
+                    }
+                }
+
+                Console.WriteLine(stops);
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+    }
+}
---
+++ 02. C#-Fundamentals/04. Exams/02. Final Exam/02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(=|\/)(?<location>[A-Z][A-Za-z]{2,})\1";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+
+            var matches = regex.Matches(input);
+            int points = 0;
+            List<string> destinations = new List<string>();
+
+            foreach (Match place in matches)
+            {
+                string destination = place.Groups[2].Value;
+                destinations.Add(destination);
+
+                points += destination.Length;
+            }
+            Console.Write($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine();
+            Console.WriteLine($"Travel Points: {points}");
+
+
+        }
+    }
+}
