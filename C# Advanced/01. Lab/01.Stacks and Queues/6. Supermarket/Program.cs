603e2848884ae98a4a133b81443c945cb9284227
Теодор Вълчев <teo.thenx@gmail.com>
2021-06-06 23:16:17 +0300

    Advance Course Softuni

---
+++ C# Advanced/01. Lab/01.Stacks and Queues/6. Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Queue<string> names = new Queue<string>();
+
+
+            while (name != "End")
+            {
+                names.Enqueue(name);
+
+                if (name == "Paid")
+                {
+                    for (int i = 0; i <= names.Count; i++)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                    names.Clear();
+
+                }
+
+
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
