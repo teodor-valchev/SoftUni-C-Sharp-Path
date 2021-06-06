4f5745548a4de76cbaaa7c3e00556a468a7b71d8
Теодор Вълчев <stu2001261001@uni-plovdiv.bg>
2021-06-06 19:41:46 +0300

    add till 6

---
+++ 03. C# Advanced/01. Lab/01.Stacks and Queues/3. Simple Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+
+            Stack<string> calc = new Stack<string>(input);
+
+            while (calc.Count > 1)
+            {
+                int firstNum = int.Parse(calc.Pop());
+                string sign = calc.Pop();
+                int secondNum = int.Parse(calc.Pop());
+
+                if (sign == "+")
+                {
+                    int result = firstNum + secondNum;
+                    calc.Push(result.ToString());
+                }
+                else if (sign == "-")
+                {
+                    int result = firstNum - secondNum;
+                    calc.Push(result.ToString());
+                }
+            }
+
+            Console.WriteLine(calc.Pop());
+        }
+    }
+}
