f3467c10cb21be880ef54986cc5c2a35044631d4
Теодор Вълчев <teo.thenx@gmail.com>
2021-06-20 13:41:22 +0300

    add

---
+++ 03. C# Advanced/01. Lab/01.Stacks and Queues/1. Reverse Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            Stack<char> chStack = new Stack<char>(input);
+
+        
+
+            while (chStack.Count > 0)
+            {
+                Console.Write(chStack.Pop());
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
