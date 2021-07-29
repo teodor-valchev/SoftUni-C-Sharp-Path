9bc6b73620a951f897cfa0e1777e8712b6d42959
Теодор Вълчев <teo.thenx@gmail.com>
2021-07-29 19:23:08 +0300

    add

---
+++ 03. C# Advanced/01. Lab/05.Defining Classes/4. Car Engine And Tires/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuanity = 200;
+            FuelConsumption = 10;
+
+        }
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car(string make, string model, int year, double fuelquantuty, double fuelConsumption)
+            : this(make, model, year)
+        {
+
+            FuelConsumption = fuelConsumption;
+            FuelQuanity = fuelquantuty;
+        }
+
+        public Car(string make, string model, int year, double fuelquantuty, double fuelConsumption,Tire[] tires,Engine engine)
+         : this(make, model, year,fuelConsumption,fuelquantuty)
+        {
+            Engine = engine;
+            Tires = tires;
+          
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public double FuelQuanity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+    }
+}
---
+++ 03. C# Advanced/01. Lab/05.Defining Classes/4. Car Engine And Tires/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tires = new Tire[4]
+            {
+                new Tire(1,2),
+                new Tire(1,2.9),
+                new Tire(1,2.54),
+                new Tire(1,25),
+            };
+            var engine = new Engine(233, 4444.4);
+
+            Car Lambo = new Car("Lamborgini", "Aventador", 2088, 343, 3, tires, engine);
+            Console.WriteLine($"Horse power: "+ Lambo.Engine.HorsePower);
+
+            foreach (var tire in Lambo.Tires)
+            {
+                Console.WriteLine($"{tire.Year} -- {tire.Pressuare}");
+            }
+        }
+    }
+}
