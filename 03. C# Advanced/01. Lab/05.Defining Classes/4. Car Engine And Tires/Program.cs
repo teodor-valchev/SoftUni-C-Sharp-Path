5768d2ba829974707adc7ff9f7bafdfbc3914ab1
Теодор Вълчев <teo.thenx@gmail.com>
2021-10-03 18:08:04 +0300

    example 4

--- 03. C# Advanced/01. Lab/05.Defining Classes/4. Car Engine And Tires/Program.cs
+++ 03. C# Advanced/01. Lab/05.Defining Classes/4. Car Engine And Tires/Program.cs
@@ -2,86 +2,86 @@
 
 namespace CarManufacturer
 {
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private int horsePower;
+        private double cubicCapacity;
+        private double pressure;
+
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuanity, double fuelConsumption)
+         : this(make, model, year)
+        {
+            FuelConsumption = fuelConsumption;
+            FuelQuantity = fuelQuanity;
+        }
+
+        public Car(string make, string model, int year, double fuelQuanity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuanity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+    }
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+    }
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+    }
+
     public class StartUp
     {
-
-        public class Car
-        {
-            private string make;
-            private string model;
-            private int year;
-            private double fuelQuantity;
-            private double fuelConsumption;
-            private int horsePower;
-            private double cubicCapacity;         
-            private double pressure;
-
-
-            public Car()
-            {
-                Make = "VW";
-                Model = "Golf";
-                Year = 2025;
-                FuelQuantity = 200;
-                FuelConsumption = 10;
-            }
-            public Car(string make, string model, int year)
-                : this()
-            {
-                Make = make;
-                Model = model;
-                Year = year;
-            }
-            public Car(string make, string model, int year, double fuelQuanity, double fuelConsumption)
-             : this(make, model, year)
-            {
-                FuelConsumption = fuelConsumption;
-                FuelQuantity = fuelQuanity;
-            }
-
-            public Car(string make, string model, int year, double fuelQuanity, double fuelConsumption, Engine engine, Tire[] tires)
-                : this(make, model, year, fuelQuanity, fuelConsumption)
-            {
-                Engine = engine;
-                Tires = tires;
-
-            }
-            public string Make { get; set; }
-            public string Model { get; set; }
-            public int Year { get; set; }
-            public double FuelQuantity { get; set; }
-            public double FuelConsumption { get; set; }
-            public Engine Engine { get; set; }
-
-            public Tire[] Tires { get; set; }
-
-        }
-        public class Engine
-        {
-            private int horsePower;
-            private double cubicCapacity;
-            public Engine(int horsePower, double cubicCapacity)
-            {
-                HorsePower = horsePower;
-                CubicCapacity = cubicCapacity;
-            }
-
-            public int HorsePower { get; set; }
-            public double CubicCapacity { get; set; }
-        }
-        public class Tire
-        {
-            private int year;
-            private double pressure;
-
-            public Tire(int year, double pressure)
-            {
-                Year = year;
-                Pressure = pressure;
-            }
-            public int Year { get; set; }
-            public double Pressure { get; set; }
-        }
 
         public static void Main(string[] args)
         {
