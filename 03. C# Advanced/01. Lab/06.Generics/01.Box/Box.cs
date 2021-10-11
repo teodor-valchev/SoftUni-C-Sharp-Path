aed12f2a29e0d0d52ecb610195b3b22f1001b922
Теодор Вълчев <teo.thenx@gmail.com>
2021-10-11 20:02:53 +0300

    add

---
+++ 03. C# Advanced/01. Lab/06.Generics/01.Box/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+   public class Box<T>
+    {
+        public Box()
+        {
+            Store = new List<T>();
+        }   
+        public List<T> Store { get; set; }
+        public int Count => Store.Count;
+        public T Remove()
+        {
+            T element = Store[Store.Count - 1];
+            Store.RemoveAt(Store.Count - 1); 
+
+            return element;
+            
+        }
+        public void Add(T element)
+        {
+            Store.Add(element);
+        }
+
+        public override string ToString()
+        {
+            return $"{Store}";
+        }
+    }
+}
---
+++ 03. C# Advanced/01. Lab/06.Generics/01.Box/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+            box.Add("papp");
+            box.Add("ada");
+            box.Add("adas");
+          
+        }
+    }
+}
