07b28e4938f990e1ef23c33cbc4a17a854d0ee25
Теодор Вълчев <teo.thenx@gmail.com>
2021-10-28 19:14:21 +0300

    add lab

---
+++ C# OOP/01.Lab/01.Inheritance/04.RandomList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+  public class StartUp 
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("boi");
+            list.Add("bos");
+            list.Add("bddda");
+            list.Add("ada");
+            list.Add("dawd");
+            
+            Console.WriteLine(list.ReturnRandomElement(list));
+        }
+    }
+}
---
+++ C# OOP/01.Lab/01.Inheritance/05.StackofStrings/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    class StackOfStrings:Stack<string>
+    {
+        public bool IsEmpty() => Count> 0;
+
+        public void AddRange(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                Push(item);
+            }
+        }
+      
+    }
+}
