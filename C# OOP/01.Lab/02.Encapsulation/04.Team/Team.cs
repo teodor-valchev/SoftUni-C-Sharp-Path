432ef8d9a60a82053d2600b04d7e72956c68e266
Теодор Вълчев <teo.thenx@gmail.com>
2021-10-31 18:29:13 +0200

    Lab adding

---
+++ C# OOP/01.Lab/02.Encapsulation/04.Team/Team.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+  public  class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string teamName)
+        {
+            TeamName = teamName;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string TeamName { get; set; }
+        public IReadOnlyList<Person> FirstTeam => firstTeam;
+        public IReadOnlyList<Person> ReserveTeam => reserveTeam;
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age<40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"First team has {FirstTeam.Count} players.");
+            sb.AppendLine($"Reserve team has {ReserveTeam.Count} players.");
+
+            return sb.ToString().TrimEnd();
+          
+        }
+
+
+    }
+}
